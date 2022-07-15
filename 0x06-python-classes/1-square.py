0b28093bd2378cbe621002a8220dfcaa214d6f12
thebenjaminibe <frankynoble@gmail.com>
2022-07-15 01:36:58 -0700

    commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define an object, Square."""
+
+
+class Square:
+    """ Object Square [class]. """
+
+    def __init__(self, size):
+        """ Initialize method.
+<<<<<<< HEAD
+
+=======
+>>>>>>> 0836a46f80f6c79ff9ac641d9cb0bd7d56dbf8c8
+        Args:
+            self (class): This class
+            size (int): Size of the square
+        """
+
+        """ Instance attribute
+        """
+        self.__size = size
