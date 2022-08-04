6042d2eb7736fed4c97a044f699ab6046698f680
thebenjaminibe <frankynoble@gmail.com>
2022-08-04 02:32:57 -0700

    commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The ``inherits_from`` module"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if object is an instance of a class that inherited
+        (directly or indirectly from the specified class)
+        otherwise False
+    """
+    return type(obj) is not a_class and isinstance(obj, a_class)
