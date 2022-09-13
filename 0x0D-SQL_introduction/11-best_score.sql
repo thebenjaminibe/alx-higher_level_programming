41618e835004eb87c90cca417fda196634f85546
thebenjaminibe <frankynoble@gmail.com>
2022-09-13 02:32:44 -0700

    commit

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table table `second_table
+-- of the database hbtn_0c_0
+-- only when score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
